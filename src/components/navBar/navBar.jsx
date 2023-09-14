380d43ea1acb5c9c21791960a8a26f5e060d31e8
Tobias <tobiasramossilva@gmail.com>
2023-09-14 15:15:15 -0300

    front-#2:finishing desktop styling/feat

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,9 +1,11 @@
+import "../../App.css";
 import logo from "../../assets/logo.png";
 const NavBar = () => {
   return (
-    <nav className="navbar">
-      <div>
+    <nav className="navbar navContainer">
+      <div className="logo ms-3">
         <img src={logo} alt="Logo da navbar" />
+        <p>Free Code</p>
       </div>
 
       <ul className="nav nav-tabs">
@@ -12,10 +14,39 @@ const NavBar = () => {
             Home
           </a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
+        <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            data-bs-toggle="dropdown"
+            href="#"
+          >
             Projetos
           </a>
+          <ul className="dropdown-menu menuContainer">
+            <li>
+              <a className="dropdown-item" href="#">
+                Arco
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Kanban
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Pokedex
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider"></hr>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Mais...
+              </a>
+            </li>
+          </ul>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">
@@ -28,9 +59,9 @@ const NavBar = () => {
           </a>
         </li>
       </ul>
-      <form>
-        <button>Login</button>
-        <button>Se Cadastre</button>
+      <form className="d-flex me-3">
+        <button className="btn btn-outline-light me-2">Login</button>
+        <button className="btn btn-primary">Se Cadastrar</button>
       </form>
     </nav>
   );
